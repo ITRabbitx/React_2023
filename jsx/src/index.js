9f285dc260e63a430e822b21b27fa6983951a94c
Eva <stenbergwork@outlook.com>
2023-02-21 12:21:04 +0100

    extracting components

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,6 +1,7 @@
 // 1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root')
@@ -8,14 +9,5 @@ const el = document.getElementById('root')
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-function App() {
-    const inputType = 'number';
-    const minValue = 5;
-
-    return <input type={inputType} min={minValue} />;
-    // alternate, more condensed way of using props:
-    // return <input type="number" min={5} />;
-}  
-// 5) Show the component on the screen
-export default App;
+// 4) Show the component on the screen
 root.render(<App/>);
