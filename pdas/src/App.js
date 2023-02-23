14bf72429597f5e1c1d91f9a83de2f47cde088d3
Eva <stenbergwork@outlook.com>
2023-02-24 00:01:15 +0100

    Including images, accesability, styling with bulma

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,17 +1,47 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
-
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App() {
     return (
         <div>
-            <div> Personal Digital Assistants</div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistant</p>
+                </div>
+            </section>
+    
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title ="Alexa" 
+                                handle="@alexa99" 
+                                image={AlexaImage} 
+                                description="Alexa was made by Amazon."/>
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title ="Cortana" 
+                                handle="@cortana32" 
+                                image={CortanaImage}
+                                description="Cortana was made by Microsoft." />
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard 
+                                title ="Siri" 
+                                handle="@siri01" 
+                                image={SiriImage}
+                                description="Siri was made by Apple." />    
+                        </div>
+                    </div>
+                </section>
+            </div>
 
-            <ProfileCard title ="Alexa" handle="@alexa99" />
-            <ProfileCard title ="Cortana" handle="@cortana32" />
-            <ProfileCard title ="Siri" handle="@siri01" />
         </div>
     );    
-
 }
-
 export default App;
--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,11 +1,21 @@
-function ProfileCard({title, handle}){
-
+function ProfileCard({ title, handle, image, description }) {
     return (
-        <div>
-            <div> Title is {title}</div>
-            <div> Handle is {handle}</div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img src={image} alt="pda logo" />
+          </figure>
         </div>
-    )
-}
-
-export default ProfileCard
+  
+        <div className="card-content">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+            <p className="subtitle is-6">{handle}</p>
+          </div>
+          <div className="content">{description}</div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default ProfileCard;
