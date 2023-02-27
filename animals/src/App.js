8df9a4d83ef71c6b70836bbb7bcd6893ee777d86
Eva <stenbergwork@outlook.com>
2023-02-27 21:42:55 +0100

    Added button and event listener

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,5 +1,12 @@
 function App(){
-    return <div>Show animal list here</div>
+
+    const handleClick = () => {
+        console.log('Button was clicked')
+    };    
+
+    return <div>
+        <button onClick={handleClick}>Add Animal </button>
+    </div>
 }
 
 export default App;
