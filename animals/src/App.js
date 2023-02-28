8b0deb28ea2de0ed5a7328013a0d55403ef87f23
Eva <stenbergeva19@hotmail.com>
2023-02-28 17:40:29 +0100

    Learning about states

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,12 +1,19 @@
+import { useState } from 'react';
+
 function App(){
+    const [count, setCount] = useState(0);
+
 
     const handleClick = () => {
         console.log('Button was clicked')
     };    
 
-    return <div>
+    return (
+    <div>
         <button onClick={handleClick}>Add Animal </button>
+        <div>Number of animals: {count}</div>
     </div>
+    );
 }
 
 export default App;
