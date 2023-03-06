31489fb0233bb8282548da8f6a8af1eaa325cabd
Eva <stenbergeva19@hotmail.com>
2023-03-06 18:26:59 +0100

    loading and showing svg's

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,5 +1,24 @@
-function AnimalShow({ type }){
-    return <div>{type}</div>
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
+
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+}   
+
+function AnimalShow({ type }) {
+    return <div>
+        <img alt="animal" src={svgMap[type]} />
+    </div>
 };
 
 export default AnimalShow;
