b7efc1fe13a5f453e1370c05572354ec7a83c5b2
Eva <stenbergeva19@hotmail.com>
2023-03-08 18:48:04 +0100

    increasing image size & heart image added

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import bird from './svg/bird.svg';
 import cat from './svg/cat.svg';
 import cow from './svg/cow.svg';
@@ -16,9 +17,21 @@ const svgMap = {
 }   
 
 function AnimalShow({ type }) {
-    return <div>
+    const [clicks, setClicks] = useState(0);
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    };
+
+    return ( 
+    <div onClick={handleClick}>
         <img alt="animal" src={svgMap[type]} />
+        <img 
+        alt="heart" 
+        src={heart} 
+        style={{ width: 10 + 10 * clicks + 'px' }}
+        />
     </div>
+    );
 };
 
 export default AnimalShow;
