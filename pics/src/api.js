2547d0080fb8e14d259802372fafeed3d152b67e
Eva <stenbergeva19@hotmail.com>
2023-03-21 12:20:25 +0100

    minor correction

--- pics/src/api.js
+++ pics/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const searchImages = async () => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       headers: {
-        Authorization: 'Cient-ID aLH6dtF6y9EUm7UhAHVionvyTGX2nmAS9seDr4rL0ug',
+        Authorization: 'Client-ID aLH6dtF6y9EUm7UhAHVionvyTGX2nmAS9seDr4rL0ug',
       },   
       params: {
         query: 'cars'
