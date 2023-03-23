3b80aa56c9882e4d7e9a3de869100f7e20d4f5b7
Eva <stenbergeva19@hotmail.com>
2023-03-23 17:38:17 +0100

    data fetching & code cleanup

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,19 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       headers: {
         Authorization: 'Client-ID aLH6dtF6y9EUm7UhAHVionvyTGX2nmAS9seDr4rL0ug',
       },   
       params: {
-        query: 'cars'
+        query: term,
       },  
     });
 
-    //Caveman debugging    
-    console.log(response);
-
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;
