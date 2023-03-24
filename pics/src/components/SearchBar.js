3e64a1cc57bfe9ca366f3bbb2e747a4359376686
Eva <stenbergeva19@hotmail.com>
2023-03-24 16:14:24 +0100

    Implementing Child to Parent Communication

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,14 @@
-function SearchBar(){
-    return <div>SearchBar</div>
+function SearchBar({ onSubmit }) {
+    const handleClick = () => {
+        onSubmit('cars') //temporarily hard coded
+    };
+
+    return (
+    <div>
+        <input />
+        <button onClick={handleClick}> Click me </button>
+    </div>
+    );
 }
 
 export default SearchBar;
