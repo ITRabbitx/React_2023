4965592de42bf84cb3340979c6a6ffde9939412f
Eva <stenbergeva19@hotmail.com>
2023-03-28 21:54:38 +0200

    communicating list of images down

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,16 +1,21 @@
+import { useState} from 'react';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 import searchImages from './api';
 
 function App(){
+    const [images, setImages] = useState([]);
+
     const handleSubmit = async (term) => {
         const result = await searchImages(term);
 
-        console.log(result);
+        setImages(result);
     }
 
     return (
     <div>
         <SearchBar onSubmit={handleSubmit}/>
+        <ImageList images={images}/>
     </div>
     );
 }
