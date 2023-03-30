00ee4e83d18fc07aea8982f835d274815e5498e7
Eva <stenbergeva19@hotmail.com>
2023-03-30 14:11:29 +0200

    displaying images

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -2,7 +2,7 @@ import ImageShow from './ImageShow';
 
 function ImageList({ images }){
     const renderedImages = images.map((image) => {
-        return <ImageShow image={image}/>
+        return <ImageShow key={image.id} image={image}/>
     });
     return <div>{renderedImages}</div>
 }
