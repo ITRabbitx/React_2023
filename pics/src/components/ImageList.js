26cd7d52b51c668b43a859b2503ceb857f55619a
Eva <stenbergeva19@hotmail.com>
2023-03-30 11:59:34 +0200

    Building list of images

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,10 @@
+import ImageShow from './ImageShow';
+
 function ImageList({ images }){
-    return <div>ImageList: {images.length}</div>
+    const renderedImages = images.map((image) => {
+        return <ImageShow image={image}/>
+    });
+    return <div>{renderedImages}</div>
 }
 
 export default ImageList;
