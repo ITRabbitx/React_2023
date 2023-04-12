a9730372c5b7d76cb6c1014c4c23ede12b504823
Eva <stenbergeva19@hotmail.com>
2023-04-12 17:19:45 +0200

    Displaying the list of books

--- books/src/App.js
+++ books/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -15,7 +16,8 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
