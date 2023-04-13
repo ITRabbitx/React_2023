71429fca3b2d4bd9d20861647868339679bb3365
Eva <stenbergeva19@hotmail.com>
2023-04-13 18:25:55 +0200

    Toggling form display

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow({ book, onDelete }) {
-  const handleClick = () => {
-    onDelete(book.id);
-  };  
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleEditClick  = () => {
+      setShowEdit(!showEdit);
+    };
+
+    const handleDeleteClick = () => {
+      onDelete(book.id);
+    }; 
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit />; 
+  }  
+  
 
   return (
     <div className="book-show">
-      {book.title}
+      <div>{content}</div>
       <div className="actions">
-        <button className="delete" onClick={handleClick}>
-        Delete
+        <button className="edit" onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
         </button>
       </div>
     </div>
