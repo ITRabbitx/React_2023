2619b74acb8100f42c445cf05d9176b9684073d8
Eva <stenbergeva19@hotmail.com>
2023-04-17 22:08:21 +0200

    default form values

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,5 +1,28 @@
-function BookEdit() {
-  return <div>BookEdit</div>;
+import { useState } from 'react';
+
+function BookEdit({ book }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('New title', title);
+  };
+
+
+  return (
+  <form onSubmit={handleSubmit} className="book-edit">
+    <label>Title</label>
+    <input className="input" value={title} onChange={handleChange} />
+    <button className="button is-primary">
+      Save
+    </button>
+  </form>
+  )
 }
 
 export default BookEdit;
