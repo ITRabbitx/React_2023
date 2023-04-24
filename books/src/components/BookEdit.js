2412e57b388111eb68a1170f4fd76c6d568db63a
Eva <stenbergeva19@hotmail.com>
2023-04-24 17:30:25 +0200

    Updating the title

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function BookEdit({ book }) {
+function BookEdit({ book, onEdit }) {
   const [title, setTitle] = useState(book.title);
 
   const handleChange = (event) => {
@@ -10,7 +10,7 @@ function BookEdit({ book }) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    console.log('New title', title);
+    onEdit(book.id, title);
   };
 
 
