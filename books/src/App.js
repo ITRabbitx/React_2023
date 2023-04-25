e7bb2c423196f3d2c54de957eebd280ec435a225
Eva <stenbergeva19@hotmail.com>
2023-04-25 17:30:41 +0200

    Adding random images to books

--- books/src/App.js
+++ books/src/App.js
@@ -38,6 +38,7 @@ function App() {
 
   return (
     <div className="app">
+      <h1>Reading List</h1>
       <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
