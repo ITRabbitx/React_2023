b40f8c400b4ca9eaa311184b9bba4e9748791523
Eva <stenbergeva19@hotmail.com>
2023-05-15 20:55:47 +0200

    fetching list of books

--- books/src/App.js
+++ books/src/App.js
@@ -6,6 +6,12 @@ import axios from 'axios';
 function App() {
   const [books, setBooks] = useState([]);
 
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+
+    setBooks(response.data);
+  };
+
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
