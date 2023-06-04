99a712ec84844eac6a402833be79c127a4a96048
Eva <stenbergwork@outlook.com>
2023-06-04 23:32:57 +0200

    debugging

--- books_2/src/index.js
+++ books_2/src/index.js
@@ -2,6 +2,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import BooksContext from './context/books';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
