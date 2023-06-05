fbb1f338cb04470a830578baed8008ef397d7422
Eva <stenbergwork@outlook.com>
2023-06-05 23:26:49 +0200

    cleanup

--- books/src/index.js
+++ books/src/index.js
@@ -2,8 +2,13 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import BooksContext from './context/books';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <BooksContext.Provider value={10}>
+    <App />
+  </BooksContext.Provider>
+);
