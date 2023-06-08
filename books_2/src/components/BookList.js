edffc11423a312dd1b82b9f223e94b801e277c2c
Eva <stenbergwork@outlook.com>
2023-06-08 22:39:30 +0200

    Props and Context together

--- books_2/src/components/BookList.js
+++ books_2/src/components/BookList.js
@@ -2,12 +2,12 @@ import { useContext } from 'react';
 import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
-function BookList({ books, onDelete, onEdit }) {
-
+function BookList() {
+  const { books } = useContext(BooksContext);
 
   const renderedBooks = books.map((book) => {
     return (
-      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+      <BookShow key={book.id} book={book} />
     );
   });
 
