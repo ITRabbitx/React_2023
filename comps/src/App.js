94742398e0eed3a7daad4679e918b6a77e1ef4a3
Eva <stenbergwork@outlook.com>
2023-08-22 20:47:33 +0200

    added code to display buttons

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,6 +1,28 @@
+import Button from './Button';
+
 function App() {
-    return <div>App</div>;
+    return (
+    <div>
+        <div>
+            <Button></Button>
+        </div>    
+        <div>
+            <Button></Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+    </div>
+    )
 }
 
 export default App;
+
+
 
